491817297153d1fb15f38863927ea35af126b84c
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-17 01:10:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
     value.each do |key, value|
       new_key = key.to_s 
       new_value = value.to_s
-      puts "#{key.capitalize!}: #{value}"
+      puts "#{new_key.capitalize!}: #{new_value}"
     end 
   end 
 end
