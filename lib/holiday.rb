cb4308e0888c302c53ef269b3d6243a5bb9a5326
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-17 01:21:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,10 +50,10 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
     new_key = key.to_s
-    puts "#{new_key.capitalize!}:"
+     "#{new_key.capitalize!}:"
     value.each do |key, value|
       new_key = key.to_s 
-      puts "#{new_key.capitalize!}: #{value.flatten}"
+       "#{new_key.capitalize!}: #{value.flatten}"
     end 
   end 
 end
