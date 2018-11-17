98798b00219d5fab205afbde193870c0abe006ec
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-17 01:05:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,8 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    key.capitalize!
-    puts "#{key}:"
+    puts "#{key.capitalize!}:"
   end 
 end
 
